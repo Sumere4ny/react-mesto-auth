6901b2d9dba9588b397c40ef7c03bb1300ca76ec
sumere4ny <2kalessin@mail.ru>
2021-04-24 12:06:10 +0300

    Modify data to connect backend

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,18 @@
+import requestParams from './constants.js';
+
 class Auth {
   constructor(options) {
     this._baseUrl = options.baseUrl;
     this._headers = options.headers;
   }
-  
+
   _handleResponse(res) {
     if (!res.ok) {
-      return Promise.reject(new Error(`Ошибка: ${res.status}`));        
-    } 
+      return Promise.reject(new Error(`Ошибка: ${res.status}`));
+    }
     return res.json();
   }
-  
+
   getUserLoginInfo(token) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "GET",
@@ -20,7 +22,7 @@ class Auth {
       },
     }).then(this._handleResponse);
   }
- 
+
   signUp(email, password) {
     return fetch(`${this._baseUrl}/signup`, {
       method: "POST",
@@ -31,7 +33,7 @@ class Auth {
       }),
     }).then(this._handleResponse);
   }
-  
+
   signIn(email, password) {
     return fetch(`${this._baseUrl}/signin`, {
       method: "POST",
@@ -43,10 +45,5 @@ class Auth {
     }).then(this._handleResponse);
   }
 }
-  
-export const auth = new Auth({
-  baseUrl: "https://auth.nomoreparties.co",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+
+export const auth = new Auth(requestParams);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,9 @@
 
 const requestParams = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-19',
+  baseUrl: "https://api.sumere4ny.students.nomoredomains.icu",
   headers: {
-      authorization: 'd9b0d4bd-9467-4294-aae9-066f4b6515ac',
-      'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 };
 
 export default requestParams
