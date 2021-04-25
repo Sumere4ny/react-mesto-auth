d81991528e5e6e2b465792e55f27496b9ddf5146
sumere4ny <sumere4ny@gmail.com>
2021-04-25 20:13:31 +0300

    Change headers in constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,12 @@
 
+const token = localStorage.getItem('token')
+const jwt = token ? token : "";
+
 const requestParams = {
   baseUrl: "http://api.sumere4ny.students.nomoredomains.icu",
   headers: {
     "Content-Type": "application/json",
+    "Authorization": `Bearer ${jwt}`,
   },
 };
 
